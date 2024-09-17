19aed7f4cc00f71cb151578c7ad96554af4fc1bc
Jayaram Anandan <68646971+jayaramanandan@users.noreply.github.com>
2024-09-17 12:24:36 +0000

    client side setup done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,12 @@
-import type { Component } from 'solid-js';
+import { RouteSectionProps } from "@solidjs/router";
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
-
-const App: Component = () => {
+const App: Component<RouteSectionProps> = (props) => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <div>hello world</div>
+      {props.children}
+    </>
   );
 };
 
